ba4a1f69600a00f7eda510c1be7a585e426ef779
zhangcheck <306814290@qq.com>
2017-06-29 17:14:05 +0800

    测试

---
+++ test.js
@@ -0,0 +1,249 @@
+(function (cjs, an) {
+
+var p; // shortcut to reference prototypes
+var lib={};var ss={};var img={};
+lib.ssMetadata = [];
+
+
+// symbols:
+
+
+
+(lib.微信图片_20170629132041 = function() {
+	this.initialize(img.微信图片_20170629132041);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,948,1280);// helper functions:
+
+function mc_symbol_clone() {
+	var clone = this._cloneProps(new this.constructor(this.mode, this.startPosition, this.loop));
+	clone.gotoAndStop(this.currentFrame);
+	clone.paused = this.paused;
+	clone.framerate = this.framerate;
+	return clone;
+}
+
+function getMCSymbolPrototype(symbol, nominalBounds, frameBounds) {
+	var prototype = cjs.extend(symbol, cjs.MovieClip);
+	prototype.clone = mc_symbol_clone;
+	prototype.nominalBounds = nominalBounds;
+	prototype.frameBounds = frameBounds;
+	return prototype;
+	}
+
+
+(lib.Symbol1 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Layer 3
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f().s("#33CCCC").ss(1,1,1).p("AAAkaIAAI1");
+	this.shape.setTransform(0,0.4);
+
+	this.timeline.addTween(cjs.Tween.get(this.shape).wait(1));
+
+	// Layer 2
+	this.shape_1 = new cjs.Shape();
+	this.shape_1.graphics.f().s("#33CCCC").ss(1,1,1).p("ACdAAQAAB1gvBTQgtBThBAAQhAAAguhTQguhTAAh1QAAh0AuhTQAuhTBAAAQBBAAAtBTQAvBTAAB0g");
+	this.shape_1.setTransform(0.2,0.4);
+
+	this.timeline.addTween(cjs.Tween.get(this.shape_1).wait(1));
+
+}).prototype = getMCSymbolPrototype(lib.Symbol1, new cjs.Rectangle(-16.4,-28.8,33.3,58.6), null);
+
+
+(lib.Symbol2 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Layer 1
+	this.text = new cjs.Text("66度", "13px 'Trebuchet MS'", "#FFFFFF");
+	this.text.lineHeight = 17;
+	this.text.lineWidth = 28;
+	this.text.parent = this;
+	this.text.setTransform(-33.4,98.1);
+
+	this.instance = new lib.Symbol1();
+	this.instance.parent = this;
+	this.instance.setTransform(-11.6,75.6,1.503,1.517,-24.2,0,0,0.3,0.6);
+	this.instance.alpha = 0.461;
+
+	this.text_1 = new cjs.Text("108", "13px 'Trebuchet MS'", "#FFFFFF");
+	this.text_1.lineHeight = 17;
+	this.text_1.lineWidth = 29;
+	this.text_1.parent = this;
+	this.text_1.setTransform(-102.7,75.1);
+
+	this.text_2 = new cjs.Text("40", "13px 'Trebuchet MS'", "#FFFFFF");
+	this.text_2.lineHeight = 17;
+	this.text_2.lineWidth = 17;
+	this.text_2.parent = this;
+	this.text_2.setTransform(-4.6,67.1);
+
+	this.text_3 = new cjs.Text("43", "13px 'Trebuchet MS'", "#FFFFFF");
+	this.text_3.lineHeight = 17;
+	this.text_3.lineWidth = 100;
+	this.text_3.parent = this;
+	this.text_3.setTransform(119.5,59.1);
+
+	this.text_4 = new cjs.Text("100", "13px 'Trebuchet MS'", "#FFFFFF");
+	this.text_4.lineHeight = 17;
+	this.text_4.lineWidth = 31;
+	this.text_4.parent = this;
+	this.text_4.setTransform(36.5,-19.4,1.519,1.519);
+
+	this.text_5 = new cjs.Text("116", "13px 'Trebuchet MS'", "#FFFFFF");
+	this.text_5.lineHeight = 17;
+	this.text_5.lineWidth = 44;
+	this.text_5.parent = this;
+	this.text_5.setTransform(-34,-97.8,1.519,1.519);
+
+	this.text_6 = new cjs.Text("33", "13px 'Trebuchet MS'", "#FFFFFF");
+	this.text_6.lineHeight = 17;
+	this.text_6.lineWidth = 20;
+	this.text_6.parent = this;
+	this.text_6.setTransform(-141.1,83);
+
+	this.text_7 = new cjs.Text("49", "13px 'Trebuchet MS'", "#FFFFFF");
+	this.text_7.lineHeight = 17;
+	this.text_7.lineWidth = 19;
+	this.text_7.parent = this;
+	this.text_7.setTransform(-63.1,25.3);
+
+	this.text_8 = new cjs.Text("86", "13px 'Trebuchet MS'", "#FFFFFF");
+	this.text_8.lineHeight = 17;
+	this.text_8.lineWidth = 31;
+	this.text_8.parent = this;
+	this.text_8.setTransform(-97.6,-14.1,1.519,1.519);
+
+	this.text_9 = new cjs.Text("110", "13px 'Trebuchet MS'", "#FFFFFF");
+	this.text_9.lineHeight = 17;
+	this.text_9.lineWidth = 41;
+	this.text_9.parent = this;
+	this.text_9.setTransform(-133.6,-111.9,1.519,1.519);
+
+	this.text_10 = new cjs.Text("79", "13px 'Trebuchet MS'", "#FFFFFF");
+	this.text_10.lineHeight = 17;
+	this.text_10.lineWidth = 16;
+	this.text_10.parent = this;
+	this.text_10.setTransform(-19.1,46.6);
+
+	this.text_11 = new cjs.Text("80", "13px 'Trebuchet MS'", "#FFFFFF");
+	this.text_11.lineHeight = 17;
+	this.text_11.lineWidth = 31;
+	this.text_11.parent = this;
+	this.text_11.setTransform(-143.6,37.7,1.519,1.519);
+
+	this.text_12 = new cjs.Text("100", "13px 'Trebuchet MS'", "#FFFFFF");
+	this.text_12.lineHeight = 17;
+	this.text_12.lineWidth = 31;
+	this.text_12.parent = this;
+	this.text_12.setTransform(-200.6,-25.3,1.519,1.519);
+
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f().s("#33CCCC").ss(1,1,1).p("A3ZAJIimAAA3ZiCIAACLIAAFsIiWAAAsjyvIAARRAoSAJIvHAAADrNlIC9hPIACgBICyGUIEEAAAEAGUICqGBIDShYAEAGcICoF6ICFEtQhXAWAPBQIB1AAIACAEIACADAuPMEIAABbIEXAAAuSMEIADAAIQ+AAAuPGHIAAF9AuPSpIAAAHAuPSpIV0AAAuPNfIAAFKA5vNfICWAAIAAAGA3ZF1IAAHqIJKAAAiWF1I1DAAAItRDIAtBmIACAAIAAAEAaALCIAAGsAC0ssIAASQAM3hDIAAPn");
+	this.shape.setTransform(-55.1,-4.1);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.shape},{t:this.text_12},{t:this.text_11},{t:this.text_10},{t:this.text_9},{t:this.text_8},{t:this.text_7},{t:this.text_6},{t:this.text_5},{t:this.text_4},{t:this.text_3},{t:this.text_2},{t:this.text_1},{t:this.instance},{t:this.text}]}).wait(1));
+
+}).prototype = getMCSymbolPrototype(lib.Symbol2, new cjs.Rectangle(-222.4,-125.1,444,250.3), null);
+
+
+// stage content:
+(lib.test = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// timeline functions:
+	this.frame_0 = function() {
+		var that = this;
+		this.addEventListener('click',function(e){
+			that.lines.visible = !that.lines.visible;
+		})
+	}
+
+	// actions tween:
+	this.timeline.addTween(cjs.Tween.get(this).call(this.frame_0).wait(1));
+
+	// Layer 3
+	this.lines = new lib.Symbol2();
+	this.lines.parent = this;
+	this.lines.setTransform(340.3,266.5,1,1,0,0,0,-0.3,0.3);
+
+	this.timeline.addTween(cjs.Tween.get(this.lines).wait(1));
+
+	// Layer 1
+	this.instance = new lib.微信图片_20170629132041();
+	this.instance.parent = this;
+	this.instance.setTransform(-354,-903,1.519,1.519);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(-79,-703,1439.8,1944.1);
+// library properties:
+lib.properties = {
+	id: 'C8F31C17E165CC44A297C8641A1F2C4D',
+	width: 550,
+	height: 400,
+	fps: 24,
+	color: "#FFFFFF",
+	opacity: 1.00,
+	manifest: [
+		{src:"images/微信图片_20170629132041.jpg?1498726508199", id:"微信图片_20170629132041"}
+	],
+	preloads: []
+};
+
+
+
+// bootstrap callback support:
+
+(lib.Stage = function(canvas) {
+	createjs.Stage.call(this, canvas);
+}).prototype = p = new createjs.Stage();
+
+p.setAutoPlay = function(autoPlay) {
+	this.tickEnabled = autoPlay;
+}
+p.play = function() { this.tickEnabled = true; this.getChildAt(0).gotoAndPlay(this.getTimelinePosition()) }
+p.stop = function(ms) { if(ms) this.seek(ms); this.tickEnabled = false; }
+p.seek = function(ms) { this.tickEnabled = true; this.getChildAt(0).gotoAndStop(lib.properties.fps * ms / 1000); }
+p.getDuration = function() { return this.getChildAt(0).totalFrames / lib.properties.fps * 1000; }
+
+p.getTimelinePosition = function() { return this.getChildAt(0).currentFrame / lib.properties.fps * 1000; }
+
+an.bootcompsLoaded = an.bootcompsLoaded || [];
+if(!an.bootstrapListeners) {
+	an.bootstrapListeners=[];
+}
+
+an.bootstrapCallback=function(fnCallback) {
+	an.bootstrapListeners.push(fnCallback);
+	if(an.bootcompsLoaded.length > 0) {
+		for(var i=0; i<an.bootcompsLoaded.length; ++i) {
+			fnCallback(an.bootcompsLoaded[i]);
+		}
+	}
+};
+
+an.compositions = an.compositions || {};
+an.compositions['C8F31C17E165CC44A297C8641A1F2C4D'] = {
+	getStage: function() { return exportRoot.getStage(); },
+	getLibrary: function() { return lib; },
+	getSpriteSheet: function() { return ss; },
+	getImages: function() { return img; }
+};
+
+an.compositionLoaded = function(id) {
+	an.bootcompsLoaded.push(id);
+	for(var j=0; j<an.bootstrapListeners.length; j++) {
+		an.bootstrapListeners[j](id);
+	}
+}
+
+an.getComposition = function(id) {
+	return an.compositions[id];
+}
+
+
+
+})(createjs = createjs||{}, AdobeAn = AdobeAn||{});
+var createjs, AdobeAn;
